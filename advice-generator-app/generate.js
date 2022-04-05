5502214bd1e9d382faa9c1e81219c85d08553019
qoudri4re <qoudri4re@gmail.com>
2022-04-05 01:56:33 +0100

    ---

---
+++ advice-generator-app/generate.js
@@ -0,0 +1,12 @@
+let dice = document.getElementById("dice")
+
+
+dice.addEventListener("click", function() {
+    let fetchRes = fetch("https://api.adviceslip.com/advice");
+    fetchRes.then(res =>
+        res.json()).then(result => {
+            document.getElementById("id").innerText = result.slip.id
+            document.getElementById("advice").innerText = result.slip.advice
+    })
+    
+  });
