6a70eb1c970a93abc86bfbbd0d6298e70dec0c02
qoudri4re <qoudri4re@gmail.com>
2022-04-06 16:50:30 +0100

    fixed responsive issues with smaller screem

--- advice-generator-app/generate.js
+++ advice-generator-app/generate.js
@@ -5,7 +5,7 @@ dice.addEventListener("click", function() {
     let fetchRes = fetch("https://api.adviceslip.com/advice");
     fetchRes.then(res =>
         res.json()).then(result => {
-            document.getElementById("id").innerText = result.slip.id
+            document.getElementById("id").innerText = "Advice #" + result.slip.id
             document.getElementById("advice").innerText = result.slip.advice
     })
     
